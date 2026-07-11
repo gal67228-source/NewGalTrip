0757a5e115a77742654dd0f82e19e747136cf987
gal67228-source <gal67228@gmail.com>
2026-07-11 13:10:14 +0300

    Add files via upload

--- app/src/main/java/com/gal/familytrips/BrandingComponents.kt
+++ app/src/main/java/com/gal/familytrips/BrandingComponents.kt
@@ -12,10 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Offset
-import androidx.compose.ui.graphics.Size
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
