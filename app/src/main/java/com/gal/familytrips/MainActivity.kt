d64bd4e97d1458b53461cb1bb76a09fde6d5833a
GAL NIZRI <GALNIZRI@rafael.co.il>
2026-07-12 17:12:36 +0300

    Version Builder 2026-07-12 17:12

--- app/src/main/java/com/gal/familytrips/MainActivity.kt
+++ app/src/main/java/com/gal/familytrips/MainActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
